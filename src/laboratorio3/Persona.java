b7820ff98165daa8af9a03b506adbda8539ae7e1
Ramos-3 <142635216+Ramos-3@users.noreply.github.com>
2023-09-01 14:15:59 -0500

    Add files via upload

---
+++ src/laboratorio3/Persona.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio3;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Persona {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the DNI
+     */
+    public String getDNI() {
+        return DNI;
+    }
+
+    /**
+     * @param DNI the DNI to set
+     */
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+    private String nombre;
+    private String apellido;
+    private String DNI;
+    public Persona(String nombre, String apellido, String DNI){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.DNI=DNI;
+    }
+}
